4047be784ae1ad9ff289709df97e38b4a6a57c95
Israel <israelias94@gmail.com>
2020-05-11 23:02:08 -0400

    ECS-12 add discount percentage to product model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_25_164902) do
+ActiveRecord::Schema.define(version: 2020_05_12_021633) do
 
   create_table "action_mailbox_inbound_emails", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "status", default: 0, null: false
@@ -514,6 +514,7 @@ ActiveRecord::Schema.define(version: 2020_04_25_164902) do
     t.boolean "promotionable", default: true
     t.string "meta_title"
     t.integer "vendor_id"
+    t.decimal "discount", precision: 8, scale: 2
     t.index ["available_on"], name: "index_spree_products_on_available_on"
     t.index ["deleted_at"], name: "index_spree_products_on_deleted_at"
     t.index ["discontinue_on"], name: "index_spree_products_on_discontinue_on"
