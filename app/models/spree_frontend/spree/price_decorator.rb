5f3de5dac4c43f9cf9b32db7fb1f4bdc0c5b3e07
Hegel Trigo <hegeltrigo@gmail.com>
2020-05-18 10:40:15 -0500

    comment decorator

--- app/models/spree_frontend/spree/price_decorator.rb
+++ app/models/spree_frontend/spree/price_decorator.rb
@@ -1,18 +1,18 @@
-module SpreeFrontend
-  module Spree
-    module PriceDecorator
-      def amount
-        unless variant.no_discount_mode || variant.product.no_discount_mode
-          if variant.product.discount
-            return self[:amount] * (1 - variant.product.discount / 100)
-          end
-        end
-        self[:amount]
-      end
+# module SpreeFrontend
+#   module Spree
+#     module PriceDecorator
+#       def amount
+#         unless variant.no_discount_mode || variant.product.no_discount_mode
+#           if variant.product.discount
+#             return self[:amount] * (1 - variant.product.discount / 100)
+#           end
+#         end
+#         self[:amount]
+#       end
 
-    end
-  end
-end
+#     end
+#   end
+# end
 
 
-::Spree::Price.prepend SpreeFrontend::Spree::PriceDecorator if ::Spree::Price.included_modules.exclude?(SpreeFrontend::Spree::PriceDecorator)
+# ::Spree::Price.prepend SpreeFrontend::Spree::PriceDecorator if ::Spree::Price.included_modules.exclude?(SpreeFrontend::Spree::PriceDecorator)
