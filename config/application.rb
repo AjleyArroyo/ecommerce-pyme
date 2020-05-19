c6459c9f9093f1a502541a09a4b391e2e3a8f7c4
Danae <danna.android@gmail.com>
2020-05-19 13:06:17 -0400

    Revert "Merged feature/ECS-14 into develop"
    This reverts commit c3ef9cc89688c067ce22c466a5a3d60323669c7a.

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Marketone
   class Application < Rails::Application
     Raven.configure do |config|
       config.dsn = ENV['sentry_dsn']
+      config.environments = %w(test production)
     end
 
 
@@ -26,7 +27,6 @@ module Marketone
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
