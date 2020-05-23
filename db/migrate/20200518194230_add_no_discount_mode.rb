6efe9259ee9c98edf69f7ffca211bc774677667f
Israel <israelias94@gmail.com>
2020-05-22 21:19:21 -0400

    fix bad migration

--- db/migrate/20200518194230_add_no_discount_mode.rb
+++
@@ -1,6 +0,0 @@
-class AddNoDiscountMode < ActiveRecord::Migration[6.0]
-  def change
-    add_column :spree_products, :no_discount_mode, :boolean, default: false
-    add_column :spree_variants, :no_discount_mode, :boolean, default: false
-  end
-end
