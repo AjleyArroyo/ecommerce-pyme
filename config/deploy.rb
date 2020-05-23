9b6d17f4407414ba0bde8ce0fa0b4a00c4ee1a21
Israel <israelias94@gmail.com>
2020-05-22 21:02:52 -0400

    add application.yml to capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,7 +30,7 @@ set :format,          :pretty
 # set :log_level,     :debug
 set :keep_releases, 5
 
-append :linked_files, 'config/database.yml', 'config/master.key'
+append :linked_files, 'config/database.yml', 'config/master.key', 'config/application.yml'
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads', 'storage'
 
 set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
