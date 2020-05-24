32aace251267fd184dbec617e253c22bf3bf9365
Jorge Leandro Dorado Cuellar <jleandrodorado@gmail.com>
2020-05-24 15:45:30 -0400

    Add changes of master

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -14,6 +14,7 @@ Spree.config do |config|
   # Uncomment to stop tracking inventory levels in the application
   # config.track_inventory_levels = false
   config.products_per_page = 12
+  config.logo = 'caimango.png'
 end
 
 # Configure Spree Dependencies
