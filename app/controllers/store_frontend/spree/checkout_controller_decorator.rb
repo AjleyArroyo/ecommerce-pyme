b0e0c5647d6b3a872ef88557b329b4338b247dc8
Jorge Leandro Dorado Cuellar <jleandrodorado@gmail.com>
2020-06-12 23:53:33 -0400

    Add validations every time

--- app/controllers/store_frontend/spree/checkout_controller_decorator.rb
+++ app/controllers/store_frontend/spree/checkout_controller_decorator.rb
@@ -2,44 +2,8 @@ module StoreFrontend
   module Spree
     module CheckoutControllerDecorator
 
-      def self.prepended(base)
-        base.after_action :normalize_addresses, only: [:update,:edit]
-        base.before_action :set_addresses, only: [:update,:edit]
-      end
-
-      def set_addresses
-        return unless params[:order] && params[:state] == 'address'
-        if params[:order][:ship_address_id].to_i > 0
-          params[:order].delete(:ship_address_attributes)
-          ::Spree::Address.find(params[:order][:ship_address_id]).user_id != try_spree_current_user&.id && raise('Frontend address forging')
-        else
-          params[:order].delete(:ship_address_id)
-        end
-        if params[:order][:bill_address_id].to_i > 0
-          params[:order].delete(:bill_address_attributes)
-          ::Spree::Address.find(params[:order][:bill_address_id]).user_id != try_spree_current_user&.id && raise('Frontend address forging')
-        else
-          params[:order].delete(:bill_address_id)
-        end
-      end
-
-      def normalize_addresses
-        return unless params[:state] == 'address' && @order.bill_address_id && @order.ship_address_id
-        # ensure that there is no validation errors and addresses were saved
-        return unless @order.bill_address && @order.ship_address
-        bill_address = @order.bill_address
-        ship_address = @order.ship_address
-        if @order.bill_address_id != @order.ship_address_id && bill_address == ship_address
-          @order.update_column(:bill_address_id, ship_address.id)
-          bill_address.destroy
-        elsif params[:save_user_address]
-          bill_address.update_attribute(:user_id, try_spree_current_user&.id)
-        end
-        ship_address.update_attribute(:user_id, try_spree_current_user&.id) if params[:save_user_address]
-      end
-
       def before_address
-        headers['Last-Modified'] = Time.now.httpdate
+        @order.reload
         @order.ship_address ||= ::Spree::Address.new(country: ::Spree::Country.default, skip_validation: false)
       end
 
--- app/models/spree_frontend/spree/address_decorator.rb
+++ app/models/spree_frontend/spree/address_decorator.rb
@@ -1,18 +1,44 @@
 module SpreeFrontend
   module Spree
     module AddressDecorator
+      
       ADDRESS_FIELDS   = %w(firstname lastname company phone dni address1 address2 country state city)
       
       def self.prepended(base)
-        base.with_options unless: :skip_validation do
-          base.validates :phone, :dni, presence: true, numericality: true
-          base.validates :firstname, :lastname, presence: true
-        end
-        base.with_options if: :skip_validation do
-          base.clear_validators!
-          base.validates :nit, :social_reason, presence: true
-        end
+        base.after_initialize :with_nit
+        # base.after_find :with_nit
+        base.validates :phone, :dni, presence: true, numericality: true, unless: :skip_validation  
       end
+      
+      def skip_validators
+        self.class.clear_validators!
+        self.class.validates :nit, :social_reason, presence: true
+      end
+      
+      def with_nit
+        if self.skip_validation.presence or @skip_validation.presence
+          skip_validators
+          return true
+        else
+          unless self.skip_validation
+            self.class.clear_validators!
+            self.class.validates :firstname, :lastname, :country, :state, :phone, :dni, :address1, :city, presence: true
+            self.class.validates :phone, :dni, numericality: true
+          end 
+        end
+        false
+      end
+
+      # def self.prepended(base)
+      #   base.with_options unless: :skip_validation do
+      #     base.validates :phone, :dni, presence: true, numericality: true
+      #     base.validates :firstname, :lastname, presence: true
+      #   end
+      #   base.with_options if: :skip_validation do
+      #     base.clear_validators!
+      #     base.validates :nit, :social_reason, presence: true
+      #   end
+      # end
 
       def dni
         return 123123 if self[:country_id] == 233
